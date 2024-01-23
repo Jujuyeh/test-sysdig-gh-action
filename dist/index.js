6bab3191e942acf177ea8285484aee3c1e3f7e4c
Jujuyeh <jujuyeh@protonmail.com>
2024-01-23 14:53:04 +0100

    Add annotations

--- dist/index.js
+++ dist/index.js
@@ -199,20 +199,15 @@ async function processScanResult(result) {
 
   if (report) {
 
-    let evaluationResults;
-    if (report.scanReport) {
-      try {
-        let digest = report.result.metadata.digest;
-        let tag = report.result.metadata.pullString;
-        let imageId = report.result.metadata.imageId;
-        evaluationResults = report.scanReport[0][digest][tag][0].detail.result.result[imageId].result;
-      } catch (error) {
-        core.error("Error parsing results report: " + error);
-      }
+    let tag = "";
+    try {
+      tag = report.result.metadata.pullString;
+    } catch (error) {
+      core.error("Error parsing results report: " + error);
     }
 
     generateSARIFReport(report);
-    //await generateChecks(tag, scanResult, evaluationResults, vulnerabilities);
+    await generateChecks(tag, scanResult, report);
   }
 
   return result.ReturnCode == 0;
@@ -250,7 +245,7 @@ async function executeScan(envvars, flags) {
   };
 
   const catOptions = {
-    //silent: true,
+    silent: true,
     ignoreReturnCode: true,
     listeners: {
       stdout: (data) => {
@@ -262,46 +257,14 @@ async function executeScan(envvars, flags) {
     }
   }
 
-
-  //let retCode = await exec.exec(`docker run -d --entrypoint /bin/cat -ti ${dockerFlags} ${scanImage}`, null, scanOptions);
   let start = performance.now();
   let cmd = `./${cliScannerName} ${flags}`;
   core.debug("Executing: " + cmd);
   let retCode = await exec.exec(cmd, null, scanOptions);
   core.info("Image analysis took " + Math.round(performance.now() - start) + " milliseconds.");
 
-  // if (retCode != 0) {
-  //   return { ReturnCode: -1, Output: execOutput, Error: errOutput };
-  // }
-
   cmd = `cat ./${cliScannerResult}`;
-  retCode = await exec.exec(cmd, null, catOptions);
-
-  // let containerId = execOutput.trim();
-  // await exec.exec(`docker exec ${containerId} mkdir -p /tmp/sysdig-inline-scan/logs/`, null, {silent: true, ignoreReturnCode: true});
-  // await exec.exec(`docker exec ${containerId} touch /tmp/sysdig-inline-scan/logs/info.log`, null, {silent: true, ignoreReturnCode: true});
-  // let tailExec = exec.exec(`docker exec ${containerId} tail -f /tmp/sysdig-inline-scan/logs/info.log`, null, tailOptions);
-
-  // execOutput = '';
-  // let start = performance.now();
-  // let cmd = `docker exec ${containerId} /sysdig-inline-scan.sh ${runFlags}`;
-  // core.debug("Executing: " + cmd);
-  // retCode = await exec.exec(cmd, null, scanOptions);
-  // core.info("Image analysis took " + Math.round(performance.now() - start) + " milliseconds.");
-
-  // await function () {
-  //   return new Promise((resolve) => {
-  //     setTimeout(resolve, 1000);
-  //   });
-  // }();
-
-  // try {
-  //   await exec.exec(`docker stop ${containerId} -t 0`, null, {silent: true, ignoreReturnCode: true});
-  //   await exec.exec(`docker rm ${containerId}`, null, {silent: true, ignoreReturnCode: true});
-  //   await tailExec;
-  // } catch (error) {
-  //   core.info("Error stopping container: " + error);
-  // }
+  await exec.exec(cmd, null, catOptions);
 
   return { ReturnCode: retCode, Output: execOutput, Error: errOutput };
 }
@@ -433,78 +396,6 @@ function vulnerabilities2SARIFRes(data) {
   return [rules, results];
 }
 
-function vulnerabilities2SARIFRules(data) {
-  var ret = {};
-  if (data) {
-    ret = data.map(v => {
-      return {
-        id: getRuleId(v),
-        shortDescription: {
-          text: getSARIFVulnShortDescription(v),
-        },
-        fullDescription: {
-          text: getSARIFVulnFullDescription(v),
-        },
-        help: getSARIFVulnHelp(v)
-      }
-    }
-    );
-  }
-  return (ret);
-}
-
-function vulnerabilities2SARIFResults(tag, vulnerabilities) {
-  var ret = {};
-
-  if (vulnerabilities) {
-    ret = vulnerabilities.map((v) => {
-      return {
-        ruleId: getRuleId(v),
-        ruleIndex: 0,
-        level: "error",
-        message: {
-          text: getSARIFVulnShortDescription(v),
-          id: "default",
-        },
-        analysisTarget: {
-          uri: `Container image ${tag}`,
-          index: 0,
-        },
-        locations: [
-          {
-            physicalLocation: {
-              artifactLocation: {
-                uri: `Container image ${tag}`,
-              },
-              region: {
-                startLine: 1,
-                startColumn: 1,
-                endLine: 1,
-                endColumn: 1,
-                byteOffset: 1,
-                byteLength: 1,
-              },
-            },
-            logicalLocations: [
-              {
-                fullyQualifiedName: `Container image ${tag}`,
-              },
-            ],
-          },
-        ],
-        suppressions: [
-          {
-            kind: "external",
-          },
-        ],
-        baselineState: "unchanged",
-      };
-    });
-  }
-  return ret;
-}
-
-
 function getSARIFVulnShortDescription(pkg, vuln) {
   return `${vuln.name} Severity: ${vuln.severity.value} Package: ${pkg.name}`;
 }
@@ -542,11 +433,11 @@ URL: https://nvd.nist.gov/vuln/detail/${vuln.name}`,
 
 function getSARIFReportMessage(data, vuln, pkg, baseUrl) {
   return `Full image scan results in Sysdig UI: [${data.result.metadata.pullString} scan result](${data.info.resultUrl})
-  Package: [${pkg.name}](${baseUrl}/content?filter=freeText+in+(${pkg.name}\))
+  Package: [${pkg.name}](${baseUrl}/content?filter=freeText+in+(\"${pkg.name}\"))
   Package type: ${pkg.type}
   Installed Version: ${pkg.version}
   Package path: ${pkg.path}
-  Vulnerability: [${vuln.name}](${baseUrl}/vulnerabilities?filter=freeText+in+(${vuln.name}\))
+  Vulnerability: [${vuln.name}](${baseUrl}/vulnerabilities?filter=freeText+in+(\"${vuln.name}\"))
   Severity: ${vuln.severity.value}
   CVSS Score: ${vuln.cvssScore.value.score}
   CVSS Version: ${vuln.cvssScore.value.version}
@@ -567,7 +458,7 @@ function generateSARIFReport(data) {
   fs.writeFileSync("./sarif.json", JSON.stringify(sarifOutput, null, 2));
 }
 
-async function generateChecks(tag, scanResult, evaluationResults, vulnerabilities) {
+async function generateChecks(tag, scanResult, data) {
   const githubToken = core.getInput('github-token');
   if (!githubToken) {
     core.warning("No github-token provided. Skipping creation of check run");
@@ -579,7 +470,7 @@ async function generateChecks(tag, scanResult, evaluationResults, vulnerabilitie
 
   try {
     octokit = github.getOctokit(githubToken);
-    annotations = getReportAnnotations(evaluationResults, vulnerabilities)
+    annotations = getReportAnnotations(data)
   } catch (error) {
     core.warning("Error creating octokit: " + error);
     return;
@@ -626,48 +517,47 @@ async function generateChecks(tag, scanResult, evaluationResults, vulnerabilitie
   }
 }
 
-function getReportAnnotations(evaluationResults, vulnerabilities) {
-  let actionCol = evaluationResults.header.indexOf("Gate_Action");
-  let gateCol = evaluationResults.header.indexOf("Gate");
-  let triggerCol = evaluationResults.header.indexOf("Trigger");
-  let outputCol = evaluationResults.header.indexOf("Check_Output");
-  let gates = evaluationResults.rows.map(g => {
-    let action = g[actionCol];
-    let level = "notice"
-    if (action == "warn") {
-      level = "warning";
-    } else if (action == "stop") {
-      level = "failure";
-    }
-    return {
-      path: "Dockerfile",
-      start_line: 1,
-      end_line: 1,
-      annotation_level: level,
-      message: `${g[actionCol]} ${g[gateCol]}:${g[triggerCol]}\n${g[outputCol]}`,
-      title: `${g[actionCol]} ${g[gateCol]}`
-    }
-  });
-  let severities = {"critical":0,"high":1, "medium":2, "low":3, "negligible":4,"unknown":5}
-  let severity =  core.getInput('severity') || "unknown";
-  let uniqueReportByPackage = core.getInput('unique-report-by-package') === 'true' || false;
-  let _vulns = vulnerabilities
-  if(uniqueReportByPackage) {
-    const key = 'package'; // Show only one issue by pkg, avoiding flood of annotations
-    let _sortedVulns = _vulns.sort((a, b) => severities[b.severity.toLowerCase()] - severities[a.severity.toLowerCase()]);
-    _vulns = [...new Map(_sortedVulns.map(item => [item[key], item])).values()];
+function getRuleMessage(rule, packages) {
+
+  let table = `
+| Severity | Package | CVSS Score | CVSS Version | CVSS Vector | Fixed Version | Exploitable |
+| -------- | ------- | ---------- | ------------ | ----------- | ------------- | ----------- |
+`
+
+  for (const failure in rule.failures) {
+    let pkgIndex = failure.pkgIndex;
+    let vulnInPkgIndex = failure.vulnInPkgIndex;
+
+    let pkg = packages[pkgIndex];
+    let vuln = pkg.vuln[vulnInPkgIndex];
+    table += `\n| ${vuln.severity.value} | ${pkg.name} | ${vuln.cvssScore.value.score} | ${vuln.cvssScore.value.version} | ${vuln.cvssScore.value.vector} | ${pkg.suggestedFix || "None"} | ${vuln.exploitable} |`
   }
-  let vulns = _vulns.filter(v => severities[v.severity.toLowerCase()] <=  severities[severity.toLowerCase()]).map(v => {
-    return {
-      path: "Dockerfile",
-      start_line: 1,
-      end_line: 1,
-      annotation_level: "warning", //Convert v.severity to notice, warning, or failure?
-      message: `${v.vuln} Severity=${v.severity} Package=${v.package} Type=${v.package_type} Fix=${v.fix} Url=${v.url}`,
-      title: `Vulnerability found: ${v.vuln}`
-    }
+
+  return table;
+}
+
+function getReportAnnotations(data) {
+  let policyEvaluations = data.result.policyEvaluations;
+  let packages = data.result.packages;
+  let gates = [];
+
+  policyEvaluations.forEach(policy => {
+    policy.bundles.forEach(bundle => {
+      bundle.rules.forEach(rule => {
+        if (rule.evaluationResult != "passed") {
+          gates.push({
+            path: "Dockerfile",
+            start_line: 1,
+            end_line: 1,
+            annotation_level: "error",
+            message: getRuleMessage(rule, packages),
+            title: `${rule.description}`
+          });
+        }
+      });
+    });
   });
-  return gates.concat(vulns);
+  return gates;
 }
 
 module.exports = {
